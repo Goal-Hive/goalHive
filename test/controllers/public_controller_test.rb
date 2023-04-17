5dd954902a36c1eaf55470fdb6bdbdfd89fc7a73
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-04-17 15:35:04 +0200

    build(basic structure): setup RuboCop

--- test/controllers/public_controller_test.rb
+++ test/controllers/public_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class PublicControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get public_index_url
     assert_response :success
   end
