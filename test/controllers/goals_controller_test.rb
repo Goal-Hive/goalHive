cff16d85cc169b93023a471afb536b3e1a89b004
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-04-24 19:38:48 +0200

    fix(GH-10): rubocop

--- test/controllers/goals_controller_test.rb
+++ test/controllers/goals_controller_test.rb
@@ -1,45 +1,45 @@
-require "test_helper"
+require 'test_helper'
 
 class GoalsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @goal = goals(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get goals_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_goal_url
     assert_response :success
   end
 
-  test "should create goal" do
-    assert_difference("Goal.count") do
+  test 'should create goal' do
+    assert_difference('Goal.count') do
       post goals_url, params: { goal: { description: @goal.description, motivation: @goal.motivation } }
     end
 
     assert_redirected_to goal_url(Goal.last)
   end
 
-  test "should show goal" do
+  test 'should show goal' do
     get goal_url(@goal)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_goal_url(@goal)
     assert_response :success
   end
 
-  test "should update goal" do
+  test 'should update goal' do
     patch goal_url(@goal), params: { goal: { description: @goal.description, motivation: @goal.motivation } }
     assert_redirected_to goal_url(@goal)
   end
 
-  test "should destroy goal" do
-    assert_difference("Goal.count", -1) do
+  test 'should destroy goal' do
+    assert_difference('Goal.count', -1) do
       delete goal_url(@goal)
     end
 
