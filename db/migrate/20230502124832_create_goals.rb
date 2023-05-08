f7276ac50a3d944a72e649f73f7ed7911cde7027
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-05-08 11:29:28 +0200

    feat(GH-9): add required relations of goal and user

--- db/migrate/20230502124832_create_goals.rb
+++ db/migrate/20230502124832_create_goals.rb
@@ -2,7 +2,7 @@ class CreateGoals < ActiveRecord::Migration[7.0]
   def change
     create_table :goals do |t|
       t.string :description
-      t.string :motivaiton
+      t.string :motivation
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
