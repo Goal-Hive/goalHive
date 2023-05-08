24a5130330ad7d8af6a999254cc5db614a802c6f
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-05-08 11:20:54 +0200

    build(GH-9): change migrations files

--- db/migrate/20230502124832_create_goals.rb
+++ db/migrate/20230502124832_create_goals.rb
@@ -2,7 +2,8 @@ class CreateGoals < ActiveRecord::Migration[7.0]
   def change
     create_table :goals do |t|
       t.string :description
-      t.string :motivation
+      t.string :motivaiton
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
