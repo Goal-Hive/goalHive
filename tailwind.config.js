c189d7ee37efcc5ce89da560915ae3411dcfe4c2
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-05-11 12:23:52 +0200

    fix: change config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,38 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js',
+    content: [
+        './app/views/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/assets/stylesheets/**/*.css',
+        './app/javascript/**/*.js',
 
-    './public/*.html',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        body: {
-          "light-mode": "#FAFAFA"
+        './public/*.html',
+        './app/views/**/*.{erb,haml,html,slim}'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                light: {
+                    text: {
+                        main: "#717171",
+                        secondary: "#A5A3A3"
+                    },
+                    body: "#FAFAFA",
+                    header: "#FBEAC8",
+                    btn: "#414FC7",
+                    sideNav: "#FAFAFA"
+                },
+            }
         },
-        header:{
-          "light-mode": "#FBEAC8"
-        },
-        btn:{
-          "light-mode":"#414FC7"
-        },
-        sideNav: {
-          "light-mode": "#FAFAFA"
-        }
-      }
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/container-queries'),
+    ]
 }
