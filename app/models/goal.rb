499f400af6b18c503ef06775a853d83a888f609d
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-05-17 17:19:00 +0200

    feat: milestone

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,6 +3,8 @@
 class Goal < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :milestones, dependent: :destroy
   validates :description, presence: true
   scope :by_category, ->(category) { where(category:) }
+  accepts_nested_attributes_for :milestones
 end
---
+++ db/migrate/20230511224441_create_milestones.rb
@@ -0,0 +1,10 @@
+class CreateMilestones < ActiveRecord::Migration[7.0]
+  def change
+    create_table :milestones do |t|
+      t.string :description
+      t.references :goal, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
