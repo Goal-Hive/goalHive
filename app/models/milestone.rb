d106266e98b08855716d4a6076c5d169f009b3b9
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-06-09 18:57:34 +0200

    refactor: add status

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,3 +1,4 @@
 class Milestone < ApplicationRecord
   belongs_to :goal
+  enum :status, [ :in_progress, :achieved, :archived ], default: :in_progress, prefix: true
 end
---
+++ db/migrate/20230609154506_add_status_to_milestones.rb
@@ -0,0 +1,5 @@
+class AddStatusToMilestones < ActiveRecord::Migration[7.0]
+  def change
+    add_column :milestones, :status, :integer, default: 0
+  end
+end
