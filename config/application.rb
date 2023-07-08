f158a4fab9e28aefa2f275fd3707591c244b1213
Your name <Your personal email>
2023-07-08 11:00:35 +0300

    render deploy

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module GoalHive
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # config.assets.enabled = true
   end
 end
