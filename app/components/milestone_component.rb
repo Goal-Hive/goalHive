8c8fb26f41ade43f8e84d2ef043b165f365139cc
Your name <Your personal email>
2023-07-08 13:15:30 +0300

    fix images

--- app/components/milestone_component.rb
+++ app/components/milestone_component.rb
@@ -9,7 +9,7 @@ class MilestoneComponent < ViewComponent::Base
   def prepare_data
     if @milestone.status_in_progress?
       @update_progress = "Mark as achieved"
-      @update_progress_img = 'markAchieved'
+      @update_progress_img = 'markAchieved.svg'
       @message_img = 'markAchieved.svg'
       @status = 'achieved'
       @message = "Work In Progress"
@@ -17,7 +17,7 @@ class MilestoneComponent < ViewComponent::Base
       @text_color = "text-light-text-lightGray"
     elsif @milestone.status_achieved?
       @update_progress = "Undo"
-      @update_progress_img = 'undoProgress'
+      @update_progress_img = 'undoProgress.svg'
       @message_img = 'achieved.svg'
       @status = 'in_progress'
       @message = "Achieved"
