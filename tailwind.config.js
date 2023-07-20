a2c2012e2be3bebf14fa4d0cc3f204bdc409861a
Your name <Your personal email>
2023-07-20 17:13:09 +0300

    build: flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         './public/*.html',
         './app/views/**/*.{erb,haml,html,slim}',
         './app/components/**/*.{erb,html}',
+        "./node_modules/flowbite/**/*.js"
     ],
     theme: {
         extend: {
@@ -23,7 +24,7 @@ module.exports = {
                         secondary: "#A5A3A3",
                         categories: "#415472",
                         details: {
-                            tabs: "rgba(65, 84, 114, 0.75)",
+                            tab: "#415472",
                             selectedTab: "#415472"
                         },
                         darkPurple: "#415472",
@@ -32,14 +33,13 @@ module.exports = {
                     },
                     body: "#FAFAFA",
                     header: "rgba(251, 234, 200, 0.7)",
-                    btn: "#414FC7",
                     sideNav: "#FAFAFA",
                     bg: {
                         lightYellow: "#FFFCEF",
+                        darkPurple: "#414FC7",
                         secondary: "#F4F6FF",
                         lightGray: "#F5F5F5",
                         achieved: "#FFF7DC",
-                        darkPurple:"#414FC7",
                         darkYellow: "#FFC700"
                     }
                 },
@@ -51,5 +51,6 @@ module.exports = {
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/container-queries'),
+        require('flowbite/plugin')
     ]
 }
