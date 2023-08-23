e5f2cdeda8eee5208f68e724a99f9e9e4ad9cd47
Your name <Your personal email>
2023-08-23 15:17:24 +0300

    fix height issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,18 @@ module.exports = {
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
+            height: {
+                "10v": "10vh",
+                "20v": "20vh",
+                "30v": "30vh",
+                "40v": "40vh",
+                "50v": "50vh",
+                "60v": "60vh",
+                "70v": "70vh",
+                "80v": "80vh",
+                "90v": "90vh",
+                "100v": "100vh",
+            },
             colors: {
                 light: {
                     text: {
