8dad86b15b68f4be2f83792b0bd3054ed09127df
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-12-19 22:03:45 +0200

    feat: toggle progress percentage

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -3,6 +3,7 @@ import {Controller} from "@hotwired/stimulus"
 // Connects to data-controller="toggle"
 export default class extends Controller {
     static targets = ["toggledContent", "toggleBtn"]
+    static values = { id: String }
 
     connect() {
         // console.log('toggle connected')
@@ -14,5 +15,11 @@ export default class extends Controller {
             part.classList.toggle('hidden')
         })
         this.toggleBtnTarget.classList.toggle('hidden')
+    }
+
+    toggleById(e) {
+        console.log(this.idValue)
+        console.log(this.element.closest(`#${this.idValue}`))
+        document.querySelector(`#${this.idValue}`).classList.toggle('hidden')
     }
 }
