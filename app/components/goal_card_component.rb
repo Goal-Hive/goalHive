c17f41d72812d748ccd56f58d95ed6eeeb7fd4df
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-12-21 13:56:30 +0200

    fix: goal card update button should be activated on milestone area click not just yellow circle

--- app/components/goal_card_component.rb
+++ app/components/goal_card_component.rb
@@ -9,17 +9,19 @@ class GoalCardComponent < ViewComponent::Base
   def generate_goal_data
     achievedCount = @goal.milestones.where(status: 'achieved').count
     allMilestones = @goal.milestones.count
-    if allMilestones != 0
-      @percentage = (achievedCount * 100) / allMilestones
-    else
-      @percentage = 0
-    end
+    @percentage = if allMilestones != 0
+                    (achievedCount * 100) / allMilestones
+                  else
+                    0
+                  end
     @milestone_reached = "#{achievedCount}/#{allMilestones}"
     @milestone_number = @goal.milestones.count - @goal.milestones.where(status: 'in_progress').count + 1
-    if allMilestones == 0
+    @next_milestone = @goal.milestones.where(status: 'in_progress')&.first
+    case allMilestones
+    when 0
       @message = "You've not set milestones for this goal."
-    elsif achievedCount == allMilestones
-      @message = "Congratulations 🥳🎉🎊"
+    when achievedCount
+      @message = 'Congratulations 🥳🎉🎊'
     end
     @status_icon = 'resume' if @goal.status_paused?
     @status_icon = 'pause' if @goal.status_active?
