c165fd3697fe2b88d013a7d8465e7a1cea92c5b3
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-01-02 19:47:13 +0200

    add(HIVE-50): default style to active category

--- app/javascript/controllers/categories/navbar_controller.js
+++ app/javascript/controllers/categories/navbar_controller.js
@@ -7,26 +7,38 @@ export default class extends Controller {
     }
 
     connect() {
+        if (!this.currIdValue) {
+            const activeCategory = document.querySelector(`[data-category-type="status"]`)
+            this.styleSelected(activeCategory)
+            this.currIdValue = activeCategory.id
+        }
     }
 
     select(e) {
-        let currCategory
-        if (e.target.dataset.categorytype === 'status') {
-            currCategory = e.currentTarget.querySelector('button')
+        let selectedCategory
+        if (e.target.dataset.categoryType === 'status') {
+            selectedCategory = e.currentTarget.querySelector('button')
         } else {
-            currCategory = e.currentTarget.firstElementChild
-        }
-        if (this.hasCurrIdValue && currCategory.id != this.currIdValue) {
-            const category = document.querySelector(`#${this.currIdValue}`)
-            category.classList.remove('selected_category')
-            category.querySelector(`[data-category-element="btn"]`)?.classList.add('hidden')
-            category.querySelector(`[data-category-element="options"]`)?.classList.add('hidden')
+            selectedCategory = e.currentTarget.firstElementChild
         }
 
-        this.currIdValue = currCategory.id
+        if (this.hasCurrIdValue && selectedCategory.id != this.currIdValue) {
+            const previous = document.querySelector(`#${this.currIdValue}`)
+            this.styleUnselected(previous)
+        }
+        this.currIdValue = selectedCategory.id
+        this.styleSelected(selectedCategory)
+    }
 
+    styleSelected = (currCategory) => {
         currCategory.classList.add('selected_category')
         currCategory.classList.remove('navbar_category_hover')
         currCategory.querySelector(`[data-category-element="btn"]`)?.classList.remove('hidden')
+    }
+
+    styleUnselected = (category) => {
+        category.classList.remove('selected_category')
+        category.querySelector(`[data-category-element="btn"]`)?.classList.add('hidden')
+        category.querySelector(`[data-category-element="options"]`)?.classList.add('hidden')
     }
 }
