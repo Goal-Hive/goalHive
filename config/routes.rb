8379ed3d4d22483450758397c4fb2e2b2cb756dc
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-01-02 12:45:03 +0200

    feat(HIVE-28): devise, omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root 'goals#index'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :goals do
     post 'add_milestone' => 'milestones#add_milestone'
