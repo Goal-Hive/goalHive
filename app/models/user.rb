777621ad43294085261a5a4e8e80908e48af407b
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-01-05 03:57:17 +0200

    deploy: cred

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable,
-         :trackable, :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         # :confirmable,
+         # :lockable,
+         # :timeoutable,
+         # :trackable,
+         # :omniauthable
 
   has_many :goals, dependent: :destroy
   has_many :categories, dependent: :destroy
