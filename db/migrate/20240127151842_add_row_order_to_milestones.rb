9bd5876cd881375caa378a019f59cb2434aebf60
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-02-01 01:22:57 +0200

    add: in progress row order

--- db/migrate/20240127151842_add_row_order_to_milestones.rb
+++ db/migrate/20240127151842_add_row_order_to_milestones.rb
@@ -1,5 +1,6 @@
 class AddRowOrderToMilestones < ActiveRecord::Migration[7.0]
   def change
     add_column :milestones, :row_order, :integer
+    Milestone.update_all('row_order = EXTRACT(EPOCH FROM created_at)')
   end
 end
