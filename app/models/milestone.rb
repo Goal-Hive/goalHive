5cb677bcc9209dc99b7d4f2875dae6a315eaf1bc
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-02-01 01:25:11 +0200

    add: in progress row order to the model file

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -6,7 +6,12 @@ class Milestone < ApplicationRecord
   enum :status, %i[in_progress achieved archived], default: :in_progress, prefix: true
 
   include RankedModel
-  ranks :row_order
+  ranks :row_order,
+        with_same: :goal_id
+
+  ranks :in_progress_row_order,
+        with_same: :goal_id,
+        scope: :status_in_progress
 
 
   def update_progress(status)
