f00ece6341b4c6882d79cf5728bb6caa828fdee9
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-02-06 10:42:59 +0200

    add: goal filter controller

---
+++ app/javascript/controllers/goals_filter_controller.js
@@ -0,0 +1,41 @@
+import {Controller} from "@hotwired/stimulus"
+import {post} from '@rails/request.js'
+
+// Connects to data-controller="goals-filter"
+export default class extends Controller {
+    static values = {
+        currentCategory: String,
+        currentStatus: String, default: 'active'
+    }
+
+    connect() {
+        // console.log('goalsFilter is connected')
+    }
+
+    filterByStatus(e) {
+        this.currentStatusValue = e.currentTarget.dataset.optionValue
+        this.filterGoals()
+    }
+
+    filterByCategory(e) {
+        this.currentCategoryValue = e.currentTarget.dataset.categoryId
+        this.filterGoals()
+    }
+
+    filterGoals() {
+        post('/goals/filter', {
+            body: JSON.stringify(
+                {
+                    by_category_and_status: {
+                        'category': this.currentCategoryValue,
+                        'status': this.currentStatusValue
+                    }
+                }
+            ),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'text/vnd.turbo-stream.html',
+            },
+        })
+    }
+}
