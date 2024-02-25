0f95667c3ee2ae386eb3c018a9a7509b9740f8b2
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-02-25 21:57:06 +0200

    fix: rename

--- app/javascript/controllers/new_goal_milestones_controller.js
+++ app/javascript/controllers/new_goal_milestones_controller.js
@@ -2,14 +2,15 @@ import {Controller} from "@hotwired/stimulus"
 
 // Connects to data-controller="new-goal-milestones"
 export default class extends Controller {
-    static targets = ["milestonesContainer", "newGoalMilestone"]
+    static targets = ["milestonesContainer", "firstMilestone"]
     static values = {
         'newGoalMilestoneTemplate': String
     }
 
     connect() {
-        this.newGoalMilestoneTemplateValue = this.newGoalMilestoneTarget.innerHTML
-        this.newGoalMilestoneTarget.innerHTML = this.replaceNewMilestoneWithTime(this.newGoalMilestoneTarget.innerHTML)
+        this.newGoalMilestoneTemplateValue = this.firstMilestoneTarget.innerHTML
+        this.firstMilestoneTarget.innerHTML = this.replaceNewMilestoneWithTime(this.firstMilestoneTarget.innerHTML)
+        this.firstMilestoneTarget.querySelector('.removeMilestone').remove()
     }
 
     addMilestone(event) {
@@ -18,18 +19,18 @@ export default class extends Controller {
         this.milestonesContainerTarget.insertAdjacentHTML("beforeend", html)
     }
 
-    replaceNewMilestoneWithTime(content = this.newGoalMilestoneTemplateValue) {
+    replaceNewMilestoneWithTime(content = this.firstMilestoneTarget) {
         const index = new Date().getTime()
         return content.replace(/new_milestone/g, index)
     }
 
     removeMilestone(event) {
         event.preventDefault()
-        event.target.parentNode.parentNode.remove()
-        const milestone = event.target.closest("[data-target='newGoalMilestone']")
-        const destroyInput = milestone.querySelector("input[name*='_destroy']")
-        destroyInput.value = "true"
-        milestone.classList.add("d-none")
+        event.target.parentNode.remove()
+        // const milestone = event.target.closest("[data-target='newGoalMilestone']")
+        // const destroyInput = milestone.querySelector("input[name*='_destroy']")
+        // destroyInput.value = "true"
+        // milestone.classList.add("d-none")
     }
 
 }
