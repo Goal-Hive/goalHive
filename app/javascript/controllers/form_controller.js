6301c942e0e0d393b77290011b540d7cf749d011
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-03-05 03:54:47 +0200

    feat: auto expand for goal desc and motivation

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form"
 export default class extends Controller {
   static values = { element: String }
-  static targets = ['scrollableParent']
+  static targets = ['scrollableParent', 'textArea']
   connect() {
     // console.log('Form controller is connected')
   }
@@ -14,6 +14,11 @@ export default class extends Controller {
       top: 0,
       behavior: "smooth" // Optional: Smooth scrolling animation
     });
+  }
+
+  autoExpand(){
+    this.textAreaTarget.style.height = 'auto';
+    this.textAreaTarget.style.height = `${this.textAreaTarget.scrollHeight}px`;
   }
   // cancel(e) {
   //   e.preventDefault()
