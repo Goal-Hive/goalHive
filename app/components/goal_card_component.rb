46c792a85fa67a8dc5af9bca52e2692b3d36693b
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-03-12 15:55:55 +0200

    refactor: goal card component

--- app/components/goal_card_component.rb
+++ app/components/goal_card_component.rb
@@ -7,23 +7,43 @@ class GoalCardComponent < ViewComponent::Base
   end
 
   def generate_goal_data
-    achievedCount = @goal.milestones.where(status: 'achieved').count
-    allMilestones = @goal.milestones.count
-    @percentage = if allMilestones != 0
-                    (achievedCount * 100) / allMilestones
-                  else
-                    0
-                  end
-    @milestone_reached = "#{achievedCount}/#{allMilestones}"
-    @milestone_number = @goal.milestones.count - @goal.milestones.where(status: 'in_progress').count + 1
-    @next_milestone = @goal.milestones.where(status: 'in_progress')&.rank(:in_progress_row_order)&.first
-    case allMilestones
+    calculate_milestone_stats
+    calculate_percentage
+    set_next_milestone
+    set_message_based_on_milestones
+    set_status_icon
+  end
+
+  def calculate_milestone_stats
+    @all_milestones = @goal.milestones.count
+    @achieved_count = @goal.milestones.where(status: 'achieved').count
+    @milestone_reached = "#{@achieved_count}/#{@all_milestones}"
+    @milestone_number = @all_milestones - @goal.milestones.where(status: 'in_progress').count + 1
+  end
+
+  def calculate_percentage
+    @percentage = @all_milestones != 0 ? (@achieved_count * 100) / @all_milestones : 0
+  end
+
+  def set_next_milestone
+    @next_milestone = @goal.milestones.where(status: 'in_progress').rank(:in_progress_row_order).first
+  end
+
+  def set_message_based_on_milestones
+    case @all_milestones
     when 0
       @message = "You've not set milestones for this goal."
-    when achievedCount
+    when @achieved_count
       @message = 'Congratulations 🥳🎉🎊'
     end
-    @status_icon = 'resume' if @goal.status_paused?
-    @status_icon = 'pause' if @goal.status_active? || @goal.status_achieved?
   end
+
+  def set_status_icon
+    if @goal.status_paused?
+      @status_icon = 'resume'
+    elsif @goal.status_active? || @goal.status_achieved?
+      @status_icon = 'pause'
+    end
+  end
+
 end
