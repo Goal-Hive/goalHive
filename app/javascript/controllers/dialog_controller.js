7765f3f85599d1af5a4a83b593d6e16166ad5e68
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-04-10 11:56:15 +0200

    feat: mobile view for details page-modal view

--- app/javascript/controllers/dialog_controller.js
+++ app/javascript/controllers/dialog_controller.js
@@ -2,24 +2,40 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dialog"
 export default class extends Controller {
-  static targets = ['modal']
+  static targets = ['modal', 'content', 'frame']
   connect() {
   }
 
-  open(){
+  open(e){
+    this.hideOldContent(e)
     this.modalTarget.showModal()
     document.body.classList.add('overflow-hidden')
   }
 
+  close(){
+    this.modalTarget.close()
+    this.clean()
+  }
+
+  hideOldContent(e){
+    this.contentTargets.map(e=> e.classList.add('hidden'))
+    const id = e.currentTarget.dataset.dialogContentId
+    document.querySelector(`#${id}`)?.classList?.toggle('hidden')
+  }
+
+  clean(){
+    this.frameTarget.innerHTML=''
+  }
+
   clickOutside(e){
     if (e.target === this.modalTarget){
-      this.modalTarget.close()
+      this.close()
     }
   }
 
   submitEnd (e) {
     if (e.detail.success) {
-      this.modalTarget.close()
+      this.close()
     }
   }
 }
